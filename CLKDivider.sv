1d17519d8da3279bbc666eb37a0b3bb1f84afda0
kquiros96 <kevinquiros96@gmail.com>
2019-10-17 15:16:29 -0600

    CLK Divider

--- CLKDivider.sv
+++ CLKDivider.sv
@@ -1,18 +1,9 @@
 module CLKDivider(clock_in, clock_out);
 
-input clock_in; // input clock 
-output clock_out; // output clock luego del dividir el input clock
-logic[27:0] counter=28'd0;
-
-parameter DIVISOR = 28'd2;
+input logic clock_in; // input clock 
+output logic clock_out = 0; // output clock luego del dividir el input clock
 
 always @(posedge clock_in)
-
-begin
- counter <= counter + 28'd1;
- if(counter>=(DIVISOR-1))
-  counter <= 28'd0;
-end
-
-assign clock_out = (counter<DIVISOR/2)?1'b0:1'b1;
+	clock_out <= !clock_out;
+	
 endmodule
